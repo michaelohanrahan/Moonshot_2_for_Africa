83e0cd5cbd2510f1447d5affb4e2aa4e99f58663
Michael O'Hanrahan <michael.c.ohanrahan@gmail.com>
2024-10-04 13:28:25 +0200

    PET

--- Snakefile
+++ Snakefile
@@ -3,8 +3,8 @@ import geopandas as gpd
 import numpy as np
 from pathlib import Path
 import scripts
-from scripts.helper import syscheck
-from scripts.helper import create_directories
+from helper import syscheck
+from helper import create_directories
 import os
 
 #TODO: finalize the workflow and how its called
@@ -19,15 +19,19 @@ DRIVE=syscheck()
 # set the working directory
 os.chdir(Path(f'{DRIVE}/moonshot2-casestudy/Wflow/africa'))
 
-dirs = create_directories(config)
-external, interdir, indir, outdir = dirs['external'], dirs['interim'], dirs['input'], dirs['output']
+data_dirs = create_directories(config)
+external, interdir, indir, outdir = data_dirs['external'], data_dirs['interim'], data_dirs['input'], data_dirs['output']
 
-# since the wildcard is not known at the start of the pipeline, we need to define a function to get the clusters
-def get_clusters(wildcards):return list(set(np.loadtxt(Path('data/2-interim/cluster_list.txt')).astype(int).tolist()))
+source_wflow = config['data']['models']['wflow']
+source_sfincs = config['data']['models']['sfincs_boundary']
+
+clusters = np.loadtxt(Path('data/2-interim/cluster_list_1844.txt')).astype(int).tolist()
 
 rule all: 
     input: 
-        expand(Path(outdir, 'models', '{cluster}', 'staticmaps', 'staticmaps.nc'), cluster=get_clusters(wildcards))
+        expand(Path(source_wflow, '{cluster}', 'staticmaps.nc'), cluster=clusters),
+        expand(Path(source_sfincs, '{cluster}', 'staticmaps.nc'), cluster=clusters),
+        expand(Path(source_sfincs, '{cluster}', 'wflow_sbm.toml'), cluster=clusters)
 
 '''
 :: Cluster the basins based on the given method
@@ -46,29 +50,29 @@ args:
         Since this is the first step below, but the indexes of the clustering are already known for the wildcards. 
 '''
 
-rule clusterbasins:
-    input: 
-        basin_geojson = config['data']['basins']
-    params: 
-        crs = config['system']['crs'],
-        method = config['methods']['cluster'],
-        touches = config['methods']['touches'],
-        plot = False,
-        savefig = True,
-        test_list = None, #can be None or list
-        fill_rings = False
-    output:models = Path(outdir, 'models', '{cluster}')#cluster=f'{{cluster}}')
-    localrule: True
-    script:
-        "scripts/01_cluster_basins.py"
+# rule clusterbasins:
+#     input: 
+#         basin_geojson = config['data']['basins']
+#     params: 
+#         crs = config['system']['crs'],
+#         method = config['methods']['cluster'],
+#         touches = config['methods']['touches'],
+#         plot = False,
+#         savefig = True,
+#         test_list = None, #can be None or list
+#         fill_rings = False
+#     output:models = Path(outdir, 'models', '{cluster}')#cluster=f'{{cluster}}')
+#     localrule: True
+#     script:
+#         "scripts/01_cluster_basins.py"
 
 #build models
 rule build_models:
     input:
         basin_geojson = 'data/2-interim/dissolved_basins.geojson',
-        model = Path(outdir, 'models', '{cluster}')
+        model = Path(source_wflow, '{cluster}', "staticmaps.nc")
     output:
-        grid = Path(outdir, 'models','{cluster}','staticmaps', 'staticmaps.nc')
+        grid = Path(source_wflow, 'models','{cluster}','staticmaps', 'staticmaps.nc')
     localrule: False #Determine the necessary recources by RAM demand and add to the profile
     script:
         "scripts/02_hydromt_wflow_build.py"
@@ -92,16 +96,64 @@ returns:
 
 '''
 
-rule build_forcing:
+# rule build_forcing:
+#     input:
+#         grid = Path(outdir, 'models','{cluster}','staticmaps', 'staticmaps.nc')
+#     params:
+#         tpf = config['data']['temp_precip_forcing'],
+#         method = config['methods']['pet_method'],
+#         tmin = config['methods']['tmin'], 
+#         tmax = config['methods']['tmax'],
+#     output:
+#         forcing = Path(outdir, 'models', '{cluster}', 'inmaps', 'inmaps_pet_prec_{params.tpf}_{params.method}_daily_*.nc')
+#     localrule: False
+#     shell:
+#         '''python scripts/00_forcing --tpf {params.tpf} --method {params.method} --tmin {params.tmin} --tmax {params.tmax}'''
+
+
+'''
+create the gaugemap from your preferred geojson file of gauges... best to make this with some manual checks to ensure the right gauges are being used
+1: src/pre/assess_discharge_data.py to perform health checks and gather all gauges to be used (assuming all data is collected in a datacatalog)
+2: scripts\pixi_run_gaugemap.bat will build a gaugemap from the output geojson and the dependency graph
+ -- you can recursively modify the ignorelist in the interim folder to remove gauges to simplify the graph
+    #TODO: automatically optimise the graph
+3: src/pre/create_discharge_data.py to to create the discharge dataset from the gauges geojson
+adding default target to false to encourage snakemake to run these rules before defining the wildcards
+'''
+
+
+rule create_gaugemap:
     input:
-        grid = Path(outdir, 'models','{cluster}','staticmaps', 'staticmaps.nc')
-    params:
-        tpf = config['data']['temp_precip_forcing'],
-        method = config['methods']['pet_method'],
-        tmin = config['methods']['tmin'], 
-        tmax = config['methods']['tmax'],
+        gauges = Path(interdir, "model_inflow_points", '{cluster}', "gauges.geojson")
     output:
-        forcing = Path(outdir, 'models', '{cluster}', 'inmaps', 'inmaps_pet_prec_{params.tpf}_{params.method}_daily_*.nc')
-    localrule: False
+        gridfile = Path(source_sfincs, '{cluster}', "staticmaps.nc"),
+        toml = Path(source_sfincs, '{cluster}', "wflow_sbm.toml")
+    params:
+        cwd=Path(os.getcwd()).as_posix(),
+        config_root=Path(source_wflow, '{cluster}'),
+        new_root=Path(source_sfincs, '{cluster}'),
+        mode="w+",
+        ignorelist=Path(interdir, "model_inflow_points", '{cluster}', "ignore_list.txt"),
+        basename="sfincs",
+        index_col="wflow_id",
+        max_dist=1000,
+        crs=config['system']['crs'],
+        config_old="wflow_sbm.toml",
+        config_new="wflow_sbm.toml"
+    localrule: True
+    cores: 1
     shell:
-        '''python scripts/00_forcing --tpf {params.tpf} --method {params.method} --tmin {params.tmin} --tmax {params.tmax}'''
+        """
+        pixi run python src/pre/update_gauges.py {params.cwd} {params.config_root} {input.gauges} \
+            --new_root "{params.new_root}" \
+            --mode "{params.mode}" \
+            --basename "{params.basename}" \
+            --index_col "{params.index_col}" \
+            --ignore_list "{params.ignorelist}" \
+            --snap_to_river True \
+            --max_dist {params.max_dist} \
+            --derive_subcatch True \
+            --crs "{params.crs}" \
+            --config_old "{params.config_old}" \
+            --config_new "{params.config_new}"
+        """
---
+++ scripts/00_PET.py
@@ -0,0 +1,238 @@
+import hydromt
+import xarray as xr
+import argparse
+import traceback
+from helper import setup_logging
+from hydromt_wflow import WflowModel as WM
+from hydromt import DataCatalog
+import os
+from pathlib import Path
+import pandas as pd
+from dask.diagnostics import ProgressBar
+import time
+from helper import syscheck
+from glob import glob
+from tqdm import tqdm
+from icecream import ic
+
+def write_forcing(ds, fn_out, freq_out='Y', chunksize=1, decimals=2, time_units="days since 1900-01-01T00:00:00", **kwargs):
+    """
+    Write dataset to NetCDF files with a specified frequency (e.g., yearly).
+    """
+    if decimals is not None:
+        ds = ds.round(decimals)
+
+    chunksizes = (chunksize, ds.dims['y'], ds.dims['x'])
+    encoding = {var: {"zlib": True, "dtype": "float32", "chunksizes": chunksizes} for var in ds.data_vars}
+
+    for v in ["time", "x", "y"]:
+        if v in ds.coords:
+            ds[v].attrs.pop("_FillValue", None)
+            encoding[v] = {"_FillValue": None}
+
+    for label, ds_gr in ds.resample(time=freq_out):
+        year = ds_gr["time"].dt.year[0].item()
+        fn_out_gr = fn_out.replace('*', str(year))
+
+        if not os.path.isdir(os.path.dirname(fn_out_gr)):
+            os.makedirs(os.path.dirname(fn_out_gr))
+        
+        delayed_obj = ds_gr.to_netcdf(fn_out_gr, encoding=encoding, mode="w", compute=False)
+        
+        with ProgressBar():
+            delayed_obj.compute(**kwargs)
+
+def vars(args):
+        variables = ["temp"]
+        if args.method == "debruin":
+            variables += ["press_msl", "kin", "kout"]
+        elif args.method == "makkink":
+            variables += ["press_msl", "kin"]
+        elif args.method == "penman-monteith_rh_simple":
+            variables += ["temp_min", "temp_max", "rh", "kin"]
+        elif args.method == "penman-monteith_tdew":
+            variables += ["temp_min", "temp_max", "wind10_u", "wind10_v", "temp_dew", "kin", "press_msl"]
+        return variables
+
+def check_dates(outfile):
+    if glob(outfile)==[]:
+        return None, None
+    else:
+        ds = xr.open_mfdataset(outfile)
+        dates = pd.to_datetime(ds.time.values)
+        tmin = dates.min()
+        tmax = dates.max()
+        return tmin, tmax
+
+def calc_pet(grid, args, l, dc):
+    DEM = dc.get_rasterdataset(
+        'era5_orography'
+    ).isel(time=0, drop=True)
+    
+    l.warning("Using ERA5 orography for DEM")
+
+    assert DEM.raster.identical_grid(grid) == True, "The grids are not identical"
+    
+    variables = vars(args)
+    if 'u10' in grid.data_vars:
+        grid = grid.rename({'u10': 'wind10_u', 'v10': 'wind10_v'})
+    if 'd2m' in grid.data_vars:
+        grid = grid.rename({'d2m': 'temp_dew'})
+
+    temp_in = hydromt.workflows.forcing.temp(
+        grid['temp'], dem_model=DEM, dem_forcing=DEM, lapse_correction=False, logger=l, freq=None)
+
+    if "penman-monteith" in args.method:
+        l.info("Calculating max and min temperature")
+        temp_max_in = hydromt.workflows.forcing.temp(
+            grid['temp_max'], dem_model=DEM, dem_forcing=DEM, lapse_correction=False, logger=l, freq=None).rename('temp_max')
+        temp_min_in = hydromt.workflows.forcing.temp(
+            grid['temp_min'], dem_model=DEM, dem_forcing=DEM, lapse_correction=False, logger=l, freq=None).rename('temp_min')
+        temp_in = xr.merge([temp_in, temp_max_in, temp_min_in])
+    year = grid.time.dt.year[0].item()
+    das = []
+    for t in tqdm(grid.time.values, desc=f'Calculating PET {year}', unit='day', position=0, total=len(grid.time.values), colour='green'):
+        day = hydromt.workflows.forcing.pet(
+            grid[variables[1:]].sel(time=slice(t,t), drop=False), temp=temp_in.sel(time=slice(t,t), drop=False), dem_model=DEM, method=args.method, press_correction=False,
+            wind_correction=False, wind_altitude=False, freq='D')
+        das.append(day)
+    pet = xr.concat(das, dim='time')
+    l.info(f"Finished calculating PET for {year}")
+    return pet
+
+def main(args):
+    os.chdir(Path(args.cwd))
+    # Read configuration and set up output directory
+    outdir = Path(args.cwd, 'data', '4-output', 'PET_global').as_posix()
+    os.makedirs(outdir, exist_ok=True)
+    outfile = Path(outdir, f'{args.tpf}_{args.method}_PET_daily_*.nc')
+    
+    # Check existing data dates
+    tmin, tmax = check_dates(str(outfile))
+    
+    # Set up logging
+    l = setup_logging('data/0-log', '00_build_PET.log')
+    l.info("Building model assuming access to deltares_data catalog")
+    l.info(f"Writing output to {outfile}")
+    
+    # Initialize data catalog
+    drive = syscheck()
+    dc = DataCatalog(f'{drive}/wflow_global/hydromt/deltares_data.yml')
+    
+    # Load input data based on existing dates or from the beginning
+    if tmin is not None:
+        l.info(f"Calculating PET from {tmin} to {tmax}")
+        grid = dc.get_rasterdataset(
+            args.tpf
+        ).sel(time=slice(tmin,tmax)).chunk({"time": 1, "latitude": -1, "longitude": -1})
+    else:
+        l.info("Calculating PET from the beginning of the dataset")
+        grid = dc.get_rasterdataset(
+            args.tpf
+        ).chunk({"time": 1, "latitude": -1, "longitude": -1})
+        
+    l.info(f"calculating PET with method: {args.method}")
+    
+    # Process data year by year
+    year_range = pd.date_range(start=grid.time.values[0], end=grid.time.values[-1], freq='Y')
+    
+    for year in year_range:
+        # Calculate PET for the current year
+        g_year = grid.sel(time=slice(year, year+pd.Timedelta(days=365)))
+        pet = calc_pet(g_year, args, l, dc)
+        
+        # Prepare output dataset with metadata
+        ds_out = xr.Dataset({'pet': pet}, attrs={'description': f'Potential evapotranspiration calculated using {args.method} method with {args.tpf} temperature forcing',
+                                                'meteo data source':f"{args.tpf}",
+                                                'method': f"{args.method}",
+                                                'dem': 'era5_orography',
+                                                'timestep': 'daily',
+                                                'lapsecorrected': 'False',
+                                                'presscorrected': 'False',
+                                                'windaltitude': 'False',
+                                                'units': 'mm/day',
+                                                'last updated': time.ctime(),
+                                                **grid.attrs})
+        
+        # Rename coordinates and write output
+        ds_out = ds_out.rename({'latitude': 'y', 'longitude': 'x'})
+        write_forcing(ds_out, str(outfile), freq_out='Y', chunksize=1, decimals=2, time_units="days since 1900-01-01T00:00:00")
+    outdir = Path(args.cwd, 'data', '4-output', 'PET_global').as_posix()
+    os.makedirs(outdir, exist_ok=True)
+    outfile = Path(outdir, f'{args.tpf}_{args.method}_PET_daily_*.nc')
+    
+    #TODO: Make it so that we append the new data to the existing file
+    tmin, tmax = check_dates(str(outfile))
+    
+    l = setup_logging('data/0-log', '00_build_PET.log')
+    l.info("Building model assuming access to deltares_data catalog")
+    l.info(f"Writing output to {outfile}")
+    drive = syscheck()
+    dc = DataCatalog(f'{drive}/wflow_global/hydromt/deltares_data.yml')
+    
+    if tmin is not None:
+        l.info(f"Calculating PET from {tmin} to {tmax}")
+        grid = dc.get_rasterdataset(
+            args.tpf
+        ).sel(time=slice(tmin,tmax)).chunk({"time": 1, "latitude": -1, "longitude": -1})
+    else:
+        l.info("Calculating PET from the beginning of the dataset")
+        grid = dc.get_rasterdataset(
+            args.tpf
+        ).chunk({"time": 1, "latitude": -1, "longitude": -1})
+        
+    l.info(f"calculating PET with method: {args.method}")
+    l.info(f"Calculating PET for {grid.time.values[0]} to {grid.time.values[-1]}")
+    l.info(f"Using {args.tpf} as the temperature forcing")
+    l.info(f"Using {args.method} as the PET method")
+    l.info(f"Using {args.dem} as the DEM")  
+    l.info(f"Using {args.timestep} as the timestep")
+    l.info(f"Lapse correction set to: {args.lapsecorrected}")
+    l.info(f"Pressure correction set to: {args.presscorrected}")
+    l.info(f"Wind altitude correction set to: {args.windaltitude}")
+    l.info(f"Using {args.lastupdated} as the lastupdated")
+    
+    year_range = pd.date_range(start=grid.time.values[0], end=grid.time.values[-1], freq='Y')
+    
+    for year in year_range:
+        g_year = grid.sel(time=slice(year, year+pd.Timedelta(days=365)))
+        pet = calc_pet(g_year, args, l, dc)
+        ds_out = xr.Dataset({'pet': pet}, attrs={'description': f'Potential evapotranspiration calculated using {args.method} method with {args.tpf} temperature forcing',
+                                                'meteo data source':f"{args.tpf}",
+                                                'method': f"{args.method}",
+                                                'dem': 'era5_orography',
+                                                'timestep': 'daily',
+                                                'lapsecorrected': 'False',
+                                                'presscorrected': 'False',
+                                                'windaltitude': 'False',
+                                                'units': 'mm/day',
+                                                'last updated': time.ctime(),
+                                                **grid.attrs})
+        
+        ds_out = ds_out.rename({'latitude': 'y', 'longitude': 'x'})
+        write_forcing(ds_out, str(outfile), freq_out='Y', chunksize=1, decimals=2, time_units="days since 1900-01-01T00:00:00")
+    
+    #write dataflags in same folder as readme
+    #if files are not full years, write a dataflag file
+    files = glob(str(outfile))
+    
+    for file in files:
+        ds = xr.open_dataset(file)
+        if ds.time.size < 365:
+            with open(os.path.join(outdir, 'dataflags.txt'), 'a') as f:
+                f.write(f'{file} does not contain a full 365 days\n')
+        ds.close()
+
+if __name__ == "__main__":
+    try:
+        drive = syscheck()
+        parser = argparse.ArgumentParser(description='Build global PET with available data')
+        parser.add_argument('--cwd', type=str, help='the current working directory', default=f'{drive}/moonshot2-casestudy/Wflow/africa')
+        parser.add_argument('--tpf', type=str, help='the temperature forcing', default='era5_daily_zarr')
+        parser.add_argument('--method', type=str, help='the method to use for PET calculation', default='penman-monteith_tdew')
+        args = parser.parse_args()
+
+        main(args)
+
+    except SystemExit:
+        traceback.print_exc()
