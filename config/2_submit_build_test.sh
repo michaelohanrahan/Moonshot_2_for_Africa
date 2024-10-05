944a633cea258be74c3d983d219106cd0825c7b4
shartgring <103518935+shartgring@users.noreply.github.com>
2024-10-05 23:36:32 +0200

    changes to filenames

--- config/2_submit_build_test.sh
+++ config/2_submit_build_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH --job-name=moonshot2_hydromt_wflow_build_all_models                                    # Job name
 #SBATCH --output=/p/moonshot2-casestudy/Wflow/h7/hydromt_wflow_build_%j.log      # Standard output and error log
-#SBATCH --time=48:00:00                                                                 # Job duration (hh:mm:ss)
-#SBATCH --partition 16vcpu
-#SBATCH --ntasks=16                                                                 # Number of tasks (analyses) to run
+#SBATCH --time=6:00:00                                                                 # Job duration (hh:mm:ss)
+#SBATCH --partition 24vcpu
+#SBATCH --ntasks=24                                                                 # Number of tasks (analyses) to run
 #SBATCH --mail-user=sebastian.hartgring@deltares.nl
 #SBATCH --mail-type=ALL
 #SBATCH --get-user-env
 
 conda init
 conda activate hydromt-wflow
-python /p/moonshot2-casestudy/Wflow/africa/scripts/02_hydromt_wflow_build.py
+python /p/moonshot2-casestudy/Wflow/africa/src/2-build/2_hydromt_wflow_build.py
